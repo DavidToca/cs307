c2f66744c40f033c2dd1782209565bdd73e1a120
David Toca <david.virusfel@gmail.com>
2015-04-08 22:08:22 -0500

    Added inbox

--- Djangoproj/PurNet/inbox/models.py
+++ Djangoproj/PurNet/inbox/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User  
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, related_name='sendedMessages', verbose_name='de')
-    recipient = models.ForeignKey(User, related_name='inboxMessages', verbose_name='para')
-    content = models.CharField(max_length=100, verbose_name='mensaje')
+    sender = models.ForeignKey(User, related_name='sendedMessages', verbose_name='from')
+    recipient = models.ForeignKey(User, related_name='inboxMessages', verbose_name='to')
+    content = models.CharField(max_length=100, verbose_name='message')
+    isDeleted = models.BooleanField(default=False, verbose_name='deleted')
 
     class Meta:
         verbose_name = 'Mensaje'
--- Djangoproj/PurNet/inbox/urls.py
+++ Djangoproj/PurNet/inbox/urls.py
@@ -4,4 +4,5 @@ urlpatterns =[
     url(r'^$', 'inbox.views.inbox', name="inbox"),
     url(r'^sended/$', 'inbox.views.sended', name="sended"),
     url(r'^trash/$', 'inbox.views.trash', name="trash"),
+    url(r'^compose/$', 'inbox.views.compose', name="compose"),
 ]
